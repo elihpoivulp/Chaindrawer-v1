20ffec49e043a58589cd1f9e3ef4950bfe794b45
Nicko Gamba <p.nickogamba@gmail.com>
2021-08-16 19:43:04 +0800

    - Added Teams controller. - Added Teams in the routes. - Made some adjustments in some css files. - Restructured base-manager.html.twig

--- source/App/Controllers/Manager/Routes.php
+++ source/App/Controllers/Manager/Routes.php
@@ -9,6 +9,7 @@ class Routes extends AbstractRoute
     public ?string $controller = 'Manager';
     public ?array $routes = [
         'dashboard' => ['action' => 'index'],
-        'profile' => ['action' => 'profile']
+        // 'profile' => ['action' => 'profile'],
+        'teams' => ['action' => 'index', 'controller' => 'Teams']
     ];
 }
