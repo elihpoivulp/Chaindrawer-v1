5d55a27eedc1210b99fba8e689cee9b30ec49cea
Nicko Gamba <p.nickogamba@gmail.com>
2021-08-22 07:20:56 +0800

    Added singular Team model

---
+++ source/Models/Team.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace CD\Models;
+
+use CD\Core\Models\DBFormModel;
+use PDO;
+
+class Team extends DBFormModel
+{
+    public ?int $AssetTeamID = null;
+    public string $AssetTeamName = '';
+    public ?int $AssetPlatFormID = null;
+    public ?int $TeamTypeID = null;
+    public ?int $PlayerID = null;
+    public ?int $ShareForAssetTeamID = null;
+    public string $Amount = '';
+
+
+    public function tableName(): string
+    {
+        return 'AssetTeams';
+    }
+
+    public function columns(): array
+    {
+        return ['*'];
+    }
+
+    public function primaryKey(): string
+    {
+        return 'AssetTeamID';
+    }
+
+    public function saveNewTeam(): bool
+    {
+        $sql = 'INSERT INTO AssetTeams (AssetTeamName, AssetTeamSlug, AssetPlatformID, TeamTypeID, PlayerID, ShareForAssetTeamID) VALUES (:name, :slug, :platform_id, :team_type_id, :player_id, :fund_id)';
+        $s = $this->db->prepare($sql);
+        $s->bindValue(':name', $this->AssetTeamName);
+        $s->bindValue(':slug', slugify($this->AssetTeamName));
+        $s->bindValue(':platform_id', $this->AssetPlatFormID, PDO::PARAM_INT);
+        $s->bindValue(':team_type_id', $this->TeamTypeID, PDO::PARAM_INT);
+        $s->bindValue(':player_id', $this->PlayerID, PDO::PARAM_INT);
+        $s->bindValue(':fund_id', $this->ShareForAssetTeamID, PDO::PARAM_INT);
+        return $s->execute();
+    }
+
+    public function insertAmount()
+    {
+        $sql = "INSERT INTO SharesForAssetTeams (SharesForAssetTeamGoalAmount) VALUES (:amt)";
+        $s = $this->db->prepare($sql);
+        if ($s->execute(['amt' => str_replace(',', '', $this->Amount)])) {
+            return $this->db->lastInsertId();
+        }
+        return false;
+    }
+
+    public function getTeamManagers()
+    {
+        $s = $this->db->prepare(
+            '
+                SELECT
+                MS.ManagerAccountID,
+                ROUND((MS.ManagerShareAmount / SFAT.SharesForAssetTeamGoalAmount) * 100, 2) AS OwnershipRate
+                FROM ManagerShares MS
+                JOIN SharesForAssetTeams SFAT ON SFAT.SharesForAssetTeamID = MS.FundForAssetTeamID
+                WHERE SharesForAssetTeamID = :id'
+        );
+        $s->execute([':id' => $this->AssetTeamID]);
+        return $s->fetchAll();
+    }
+}
