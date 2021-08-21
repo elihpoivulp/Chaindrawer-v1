b47147e7bad9a0e17ad0292325dc434ac2a74eed
Nicko Gamba <p.nickogamba@gmail.com>
2021-08-21 14:57:46 +0800

    added minor fix

--- source/Helpers/text.php
+++ source/Helpers/text.php
@@ -2,6 +2,7 @@
 function slugify($text, string $divider = '-'): string
 {
     // replace non letter or digits by divider
+    $text = str_replace("'", '', $text);
     $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
 
     // transliterate
