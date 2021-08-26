ec2f489668eb886b32644d71de18ce3bdee2c138
Nicko Gamba <p.nickogamba@gmail.com>
2021-08-26 12:39:04 +0800

    removed some properties and methods due to revision of table

--- source/Models/Person.php
+++ source/Models/Person.php
@@ -9,12 +9,11 @@ trait Person
     protected string $UserLastName = '';
     protected string $UserGender = '';
     protected string $UserEmail = '';
-    protected string $UserAddress1 = '';
+    protected string $UserAddress = '';
     protected string $UserDateAdded = '';
     protected string $UserDateLastModified = '';
     protected string $UserPhone = '';
     protected ?string $UserMiddleName = null;
-    protected ?string $UserAddress2 = null;
     protected ?string $UserPhoto = null;
 
     public function getUserID(): int
@@ -37,14 +36,9 @@ trait Person
         return $this->UserPhoto;
     }
 
-    public function getUserAddress2(): ?string
+    public function getUserAddress(): string
     {
-        return $this->UserAddress2;
-    }
-
-    public function getUserAddress1(): string
-    {
-        return $this->UserAddress1;
+        return $this->UserAddress;
     }
 
     public function getUserGender(): string
@@ -85,5 +79,11 @@ trait Person
     public function getUserMiddleName(): ?string
     {
         return $this->UserMiddleName;
+    }
+
+    public function getAvatarInitials(): string
+    {
+        // return strtoupper(join('', array_map(fn($m) => $m[0], explode(' ', $this->getUserFirstName()))) . $this->getUserLastName()[0]);
+        return strtoupper($this->getUserFirstName()[0] . $this->getUserLastName()[0]);
     }
 }
