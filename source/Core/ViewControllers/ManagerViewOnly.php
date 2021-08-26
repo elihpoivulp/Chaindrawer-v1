6d94345f1855b555e3f4bf65949c15f0e438b389
Nicko Gamba <p.nickogamba@gmail.com>
2021-08-26 10:59:50 +0800

    made the account property available throughout the views that are for manager only

--- source/Core/ViewControllers/ManagerViewOnly.php
+++ source/Core/ViewControllers/ManagerViewOnly.php
@@ -6,11 +6,24 @@ use CD\App\Controllers\Auth\Auth;
 use CD\App\Controllers\Redirect;
 use CD\Core\LoginRequiredController;
 use CD\Core\Response;
+use CD\Models\User;
 
 class ManagerViewOnly extends BaseRestrictedViewController
 {
+    protected User $account;
     public function acceptedRole(): string
     {
         return 'manager';
+    }
+
+    protected function before(): bool
+    {
+        parent::before();
+        if ($this->user->manager) {
+            $this->account = $this->user;
+            return true;
+        } else {
+            exit('This user has a Manager role but has no Manager Account');
+        }
     }
 }
