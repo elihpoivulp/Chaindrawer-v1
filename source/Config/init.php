509b0f985a100bd6f1d9dcbf3cc48d0bcd94dc47
Nicko Gamba <p.nickogamba@gmail.com>
2021-09-02 12:48:06 +0800

    moved to init file

--- source/Config/init.php
+++ source/Config/init.php
@@ -5,3 +5,5 @@ use CD\Core\Sessions\Session;
 ob_start();
 Session::start();
 error_reporting(E_ALL);
+set_error_handler('\CD\Core\Error::errorHandler');
+set_exception_handler('\CD\Core\Error::exceptionHandler');
