35353f36fe1c0cc1039718d4b50e42481c44449e
Nicko Gamba <p.nickogamba@gmail.com>
2021-09-16 15:53:03 +0800

    fixed timezone for the remote server

--- source/Config/init.php
+++ source/Config/init.php
@@ -4,6 +4,7 @@ use CD\Core\Sessions\Session;
 
 ob_start();
 Session::start();
+date_default_timezone_set('Asia/Manila');
 error_reporting(E_ALL);
 set_error_handler('\CD\Core\Error::errorHandler');
 set_exception_handler('\CD\Core\Error::exceptionHandler');
