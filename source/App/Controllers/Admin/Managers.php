ae446ec4a6677066a1b779463b26e1b3b3c71406
elihpoivulp <p.nickogamba@gmail.com>
2021-10-01 15:08:24 +0800

    fixed message titles

--- source/App/Controllers/Admin/Managers.php
+++ source/App/Controllers/Admin/Managers.php
@@ -116,13 +116,13 @@ class Managers extends AdminViewOnly
                     $user->saveNewUser($cleaned);
                     Session::setFlash('msg', 'A new user has been added!', [
                         'type' => Session::FLASH_TYPE_SUCCESS,
-                        'title' => 'Login unsuccessful',
+                        'title' => 'Success!',
                         'dismissable' => true
                     ]);
                 } catch (\Exception | \PDOException $e) {
                     Session::setFlash('toastr', 'An error has occurred while saving data. Please try again later.', [
                         'type' => Session::FLASH_TYPE_WARNING,
-                        'title' => 'Success',
+                        'title' => 'Failed',
                         'dismissable' => true
                     ]);
                 }
