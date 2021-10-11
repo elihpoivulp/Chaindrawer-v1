168b7ddfcffbd81d895c8fa8dd264efbca55c8ed
elihpoivulp <p.nickogamba@gmail.com>
2021-10-11 14:11:56 +0800

    add manager and include to a team

--- source/Models/Team.php
+++ source/Models/Team.php
@@ -60,6 +60,43 @@ class Team extends BaseDBModel
         return $s->execute();
     }
 
+
+
+    public function newShare($amt, $manager_id)
+    {
+        try {
+            $this->db->beginTransaction();
+            $sql_ms = "INSERT INTO ManagerShares (ManagerShareAmount, ManagerSharePurchaseDate, ManagerAccountID, AssetTeamID)
+VALUES (:amt, NOW(), :manager, :team);";
+
+            $s = $this->db->prepare($sql_ms);
+            $s->bindValue(':amt', $amt);
+            $s->bindValue(':manager', $manager_id);
+            $s->bindValue(':team', $this->AssetTeamID);
+            $s->execute();
+
+            $sql_ma = "UPDATE ManagerAccounts SET ManagerTotalAsset = ManagerTotalAsset + :amt WHERE ManagerAccountID = :manager;";
+
+            $s = $this->db->prepare($sql_ma);
+            $s->bindValue(':amt', $amt);
+            $s->bindValue(':manager', $manager_id);
+            $s->execute();
+
+            $sql_at = "UPDATE AssetTeams SET AssetTeamCollectedAmount = AssetTeamCollectedAmount + :amt, AssetTeamStatus = IF(AssetTeamCollectedAmount >= AssetTeams.AssetTeamValue, 1, 0), AssetTeamDateLastModified = NOW() WHERE AssetTeamID = :id;";
+
+            $s = $this->db->prepare($sql_at);
+            $s->bindValue(':amt', $amt);
+            $s->bindValue(':id', $this->AssetTeamID);
+            $s->execute();
+
+            $this->db->commit();
+
+        } catch (Exception | \PDOException $e) {
+            $this->db->rollBack();
+            throw new Exception($e->getMessage(), 500);
+        }
+    }
+
     /**
      * @throws Exception
      */
